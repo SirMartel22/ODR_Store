8ed8ac931b99d8289969e0d18c166f7c7737b8f9
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-14 14:03:45 +0100

    Created the needed pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
+import { Analytics } from '@vercel/analytics/react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 class="text-5xl font-bold underline text-blue">
         ODR_Store
       </h1>
+
+      <Analytics />
     </>
   )
 }
---
+++ src/Pages/Product_Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product_Details = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Product_Details
