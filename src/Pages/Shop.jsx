f618d48eaed188f1005888af8b2da239b459e2c4
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-19 15:45:11 +0100

    Update Added

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from '/src/Components/Layout/Hero/Hero'
 import Popular from '/src/Components/Layout/Popular/Popular'
+import Offers from '/src/Components/Layout/Offers/Offers'
+// /src/Components/Offers/Offers.jsx
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers /> 
     </div>
   )
 }
