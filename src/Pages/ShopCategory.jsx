62f1918937c314a104c979dbca96c4d77baf0510
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-23 08:29:05 +0100

    Solved the issue of all product section that wasn't showing

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -21,10 +21,12 @@ const ShopCategory = (props) => {
       </div>
       <div className="shopcategory-products">
         {all_product.map((item, i) => {
-          if (props.category === item.category) {
+          if(props.category === item.category) {
             console.log(item.category)
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}
-            />
+            return (
+              <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}
+            
+            />)
           }
         })}
 
---
+++ src/assets/all_product.js
@@ -0,0 +1,301 @@
+import img1 from '/src/assets/img1.jpg'
+import img2 from '/src/assets/img2.jpg'
+import img3 from '/src/assets/img3.jpg'
+import img4 from '/src/assets/img4.jpg'
+import img5 from '/src/assets/img5.jpg'
+import img6 from '/src/assets/img6.jpg'
+import img7 from '/src/assets/img7.jpg'
+import img8 from '/src/assets/img8.jpg'
+import img9 from '/src/assets/img9.jpg'
+import img10 from '/src/assets/img10.jpg'
+import img11 from '/src/assets/img11.jpg'
+import img12 from '/src/assets/img12.jpg'
+import img13 from '/src/assets/img13.jpg'
+import img14 from '/src/assets/img14.jpg'
+import img15 from '/src/assets/img15.jpg'
+import img16 from '/src/assets/img16.jpg'
+import img17 from '/src/assets/img17.jpg'
+import img18 from '/src/assets/img18.jpg'
+import img19 from '/src/assets/img19.jpg'
+import img20 from '/src/assets/img20.jpg'
+import img21 from '/src/assets/img21.jpg'
+import img22 from '/src/assets/img22.jpg'
+import img23 from '/src/assets/img23.jpg'
+import img24 from '/src/assets/img24.jpg'
+import img25 from '/src/assets/img25.jpg'
+import img26 from '/src/assets/img26.jpg'
+
+
+let all_product = [
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img1,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img2,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img3,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img4,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img5,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img6,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img7,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img8,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img9,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img10,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img11,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img12,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img12,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+    
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img13,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img14,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img15,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img16,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img17,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img18,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img19,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img20,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img21,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img22,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img23,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "adult",
+        image: img24,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "kid",
+        image: img25,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+      
+    {
+        id: 1,
+        name: "striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        category: "men",
+        image: img26,
+        new_price: 50.0,
+        old_price: 80.6,
+    },
+
+
+]
+
+
+export default all_product
+
