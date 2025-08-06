5cd1e3bd9cc6bd36c93b7bcb0a3bec2ed25f839a
Devtimmy <oyinloyematthew6@gmail.com>
2025-08-06 12:34:43 +0100

    Feat: Done the responsiveness of the HEro section

--- src/Components/Layout/Hero/Hero.jsx
+++ src/Components/Layout/Hero/Hero.jsx
@@ -6,25 +6,25 @@ import { MoveRight } from 'lucide-react'
 
 const Hero = () => {
   return (
-    <div className="hero">
-          <div className="hero-left">
-              <h2 className="title">ODR Fits and Wears</h2>
-              <div>
-                  <div className="hand-hand-icon">
-                       <p className="">New</p>
-                      <img src="" alt="icon image" className="" />
-                  </div>
-                  <p className="">Collections</p>
-                  <p className="">For Everyone</p>
-              </div>
-              <div className="hero-latest-btn">
-                  <div>Latest Collection</div>
-                  <MoveRight />
-              </div>
+    <div className="flex flex-col md:flex-row items-center justify-center px-12 py-18 gap-2 bg-[#895ab7ff] overflow-hidden">
+
+      <div className="hero-left flex flex-col gap-2 items-start justify-center w-[70%] ">
+          <h2 className="font-bold text-4xl text-white">ODR Fits and Wears</h2>
+          <div className="flex flex-col items-start justify-left text-left w-full mb-8">
+              <p className="text-[100px] font-[700] text-white">New</p>
+              <p className="font-bold text-3xl text-[#0f0]">Collections</p>
+              <p className=" font-bold text-3xl text-[#fff]">For Everyone</p>
+          </div>
+          <div className="hero-latest-btn flex gap-4 items-center justify-center bg-[#f3f3f3] p-4 rounded-lg w-[50%]">
+              <div className="font-bold text-1xl text-purple">Latest Collection</div>
+              <MoveRight />
+          </div>
       </div>
-      <div className="hero-right">
-        <img src={hero_image} alt="hero image" className="" />
+
+      <div className="hero-right w-[70%]">
+        <img src={hero_image} alt="hero image" className="w-full " />
       </div>
+
     </div>
   )
 }
